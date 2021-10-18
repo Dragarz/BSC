1a13f6058c37caab274bd8e53e4859e45907ef12
Dragarz <odon7ron@yandex.ru>
2021-10-18 18:33:02 +0300

    final fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,3 @@
-import com.sun.org.apache.xalan.internal.res.XSLTErrorResources;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
@@ -22,6 +20,7 @@ public class Main {
                 command = reader.readLine();
                 if (command.equals("") || command.replaceAll(" ", "").length() == 0) {
                     System.err.println("Строка пуста или состоит из пробелов! Повторите ввод: ");
+                    continue;
                 }
                 parse = command.split("\\s+");
 
@@ -32,6 +31,7 @@ public class Main {
                             TaskService.getTaskService().add(buildComm(parse));
                         }else{
                             System.err.print("Попытка создать пустую задачу повторите ввод: ");
+
                         }
                         break;
                     case PRINT:
