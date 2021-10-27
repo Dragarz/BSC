73b2fb1e4d6d5b60be19f59daa21610c1bb04c5f
Dragarz <odon7ron@yandex.ru>
2021-10-27 14:54:16 +0300

    Работа по замечаниям ч.2
    Замечание по коду:
    Класс Main.
    1. Снова нарушение правил именования (command_part_length).
    2. Для большей информативности к константам можно добавить _PATTERN
    3. Ну в принципе можно и так находить оставшуюся часть. Но с паттерном было бы поизящнее))
    Класс TaskService.
    1. Еще нарушение конвенции по именованию (TaskMap). Вообще лучше не кодировать в названии тип коллекции. Просто tasks будет лучше.
    2. if (task.length() == 5) лучше "5" заменить на константу какую-нибудь. А то просто цифра не информативно.
    3. Зачем все эти ритуалы для получения измененного сообщения в методе edit, когда можно просто получить его напрямую через command.split(" ")[2]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,47 +1,50 @@
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class Main {
 
-    static final Pattern ADD = Pattern.compile("add .+");
-    static final Pattern TOGGLE = Pattern.compile("toggle \\d+");
-    static final Pattern PRINT = Pattern.compile("print|print all");
-    static final Pattern DELETE = Pattern.compile("delete \\d+");
-    static final Pattern SEARCH = Pattern.compile("search .+");
-    static final Pattern EDIT = Pattern.compile("edit \\d+ .+");
+    static final Pattern ADD_PATTERN = Pattern.compile("add .+");
+    static final Pattern TOGGLE_PATTERN = Pattern.compile("toggle \\d+");
+    static final Pattern PRINT_PATTERN = Pattern.compile("print|print all");
+    static final Pattern DELETE_PATTERN = Pattern.compile("delete \\d+");
+    static final Pattern SEARCH_PATTERN = Pattern.compile("search .+");
+    static final Pattern EDIT_PATTERN = Pattern.compile("edit \\d+ .+");
+    static final Pattern REST_PART_PATTERN = Pattern.compile(".+ ");
     static final String QUIT = "quit";
 
 
     public static void main(String[] args) {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             String command = "";
-            int command_part_length;
-
+            Matcher restPartMatcher;
             while (!command.equals(QUIT)) {
                 command = reader.readLine().trim();
                 if (command.equals("")) {
                     System.err.println("Строка пуста или состоит из пробелов! Повторите ввод: ");
                     continue;
                 }
-                command_part_length = command.split(" ")[0].length() + 1;
+                restPartMatcher = REST_PART_PATTERN.matcher(command);
                 if (command.equals(QUIT)) {
                     System.out.println("Конец программы!");
 
-                } else if (ADD.matcher(command).matches()) {
-                    TaskService.add(command.substring(command_part_length).trim());
-                } else if (TOGGLE.matcher(command).matches()) {
-                    TaskService.toggle(command.substring(command_part_length).trim());
-                } else if (PRINT.matcher(command).matches()) {
+                } else if (ADD_PATTERN.matcher(command).matches()) {
+                    TaskService.add(restPartMatcher.replaceAll(StringUtils.EMPTY));
+                } else if (TOGGLE_PATTERN.matcher(command).matches()) {
+                    TaskService.toggle(restPartMatcher.replaceAll(StringUtils.EMPTY));
+                } else if (PRINT_PATTERN.matcher(command).matches()) {
                     TaskService.print(command);
-                } else if (DELETE.matcher(command).matches()) {
-                    TaskService.delete(command.substring(command_part_length).trim());
-                } else if (EDIT.matcher(command).matches()) {
+                } else if (DELETE_PATTERN.matcher(command).matches()) {
+                    TaskService.delete(restPartMatcher.replaceAll(StringUtils.EMPTY));
+                } else if (EDIT_PATTERN.matcher(command).matches()) {
                     TaskService.edit(command);
-                } else if (SEARCH.matcher(command).matches()) {
-                    TaskService.search(command.substring(command_part_length).trim());
+                } else if (SEARCH_PATTERN.matcher(command).matches()) {
+                    TaskService.search(restPartMatcher.replaceAll(StringUtils.EMPTY));
                 } else {
                     System.err.println("Команда не найдена повторите ввод: ");
                 }
--- src/main/java/TaskService.java
+++ src/main/java/TaskService.java
@@ -2,13 +2,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TaskService {
-    private static Map<Integer, TaskService> TaskMap = new HashMap<>();
+    private static final int LENGTH_PRINT = "print".length();
+    private static Map<Integer, TaskService> Tasks = new HashMap<>();
     private static int id = 1;
     private String task;
     private Boolean completed;
 
     private TaskService(String task) {
-        TaskMap.put(id++, this);
+        Tasks.put(id++, this);
         this.task = task;
         this.completed = false;
 
@@ -20,7 +21,7 @@ public class TaskService {
 
     public static void toggle(String id) {
 
-        TaskService task = TaskMap.get(Integer.parseInt(id));
+        TaskService task = Tasks.get(Integer.parseInt(id));
         if (task != null) {
             task.completed = !task.completed;
         } else {
@@ -30,21 +31,21 @@ public class TaskService {
 
     public static void print(String task) {
 
-        if (task.length() == 5) {
-            TaskMap.entrySet().stream()
+        if (task.length() == LENGTH_PRINT) {
+            Tasks.entrySet().stream()
                     .filter(a -> !a.getValue().completed)
                     .forEach(a -> System.out.printf("%d. [ ] %s%n", a.getKey(), a.getValue().task));
 
         } else {
-            TaskMap.forEach((key, value) -> System.out.printf("%d. %s %s%n", key, value.completed ? "[X]" : "[ ]", value.task));
+            Tasks.forEach((key, value) -> System.out.printf("%d. %s %s%n", key, value.completed ? "[X]" : "[ ]", value.task));
         }
 
     }
 
     public static void delete(String id) {
-        TaskService task = TaskMap.get(Integer.parseInt(id));
+        TaskService task = Tasks.get(Integer.parseInt(id));
         if (task != null) {
-            TaskMap.remove(Integer.parseInt(id));
+            Tasks.remove(Integer.parseInt(id));
         } else {
             System.err.println("Задачи по данному id не существует.");
         }
@@ -54,7 +55,7 @@ public class TaskService {
 
     public static void edit(String command) {
         int id = Integer.parseInt(command.split(" ")[1]);
-        TaskService task = TaskMap.get(id);
+        TaskService task = Tasks.get(id);
         if (task != null) {
             task.task = command.substring(command.indexOf(Integer.toString(id)) + 1).trim();
             task.completed = false;
@@ -64,7 +65,7 @@ public class TaskService {
     }
 
     public static void search(String substring) {
-        TaskMap.entrySet().stream()
+        Tasks.entrySet().stream()
                 .filter(a -> a.getValue().task.contains(substring))
                 .forEach(a -> System.out.printf("%d. [ ] %s%n", a.getKey(), a.getValue().task));
 
