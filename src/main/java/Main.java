61d380f777699df93495af008d95870eae032cf5
Dragarz <odon7ron@yandex.ru>
2021-10-30 21:11:25 +0300

    Regex fix
    ".+ "  to  "^\\S+(?!\\S)|^.*$"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,7 +15,7 @@ class Main {
     static final Pattern DELETE_PATTERN = Pattern.compile("delete \\d+");
     static final Pattern SEARCH_PATTERN = Pattern.compile("search .+");
     static final Pattern EDIT_PATTERN = Pattern.compile("edit \\d+ .+");
-    static final Pattern REST_PART_PATTERN = Pattern.compile(".+ ");
+    static final Pattern REST_PART_PATTERN = Pattern.compile("^\\S+(?!\\S)|^.*$");
     static final String QUIT = "quit";
 
 
